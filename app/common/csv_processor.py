d8177ce790272f326a193e8d864780c777c21988
Dennis Lee <dennislwm@yahoo.com.au>
2024-09-01 21:44:07 +0800

    Refactor CSV and Excel processor documentation and error handling in processing classes.

--- app/common/csv_processor.py
+++ app/common/csv_processor.py
@@ -9,8 +9,8 @@ class CsvProcessor(BaseProcessor, ABC):
     Initializes the CsvProcessor instance.
 
     Args:
-        rules_file_path (Path): The file path to the rules file to determine how to process the CSV.
-        input_file_path (Path): The file path to the input CSV file containing transactions.
+      rules_file_path (Path): The file path to the rules file to determine how to process the CSV.
+      input_file_path (Path): The file path to the input CSV file containing transactions.
 
     This constructor loads the rules from the provided rules file, retrieves the headers
     based on those rules, and loads the input CSV file into a DataFrame.
@@ -24,29 +24,33 @@ class CsvProcessor(BaseProcessor, ABC):
     Loads the input CSV file into a pandas DataFrame.
 
     Args:
-        file_path (Path): The file path of the input CSV file.
+      file_path (Path): The file path of the input CSV file.
 
     Returns:
-        pd.DataFrame: A DataFrame containing the data from the CSV file.
+      pd.DataFrame: A DataFrame containing the data from the CSV file.
     """
-    return pd.read_csv(file_path)
+    try:
+      ret = pd.read_csv(file_path)
+    except pd.errors.EmptyDataError:
+      raise pd.errors.EmptyDataError
+    return ret
 
   def get_header(self, rules) -> dict:
     """
     Retrieves the headers for the CSV based on the provided rules.
 
     Args:
-        rules (dict): The rules loaded from the rules file.
+      rules (dict): The rules loaded from the rules file.
 
     Returns:
-        dict: A dictionary of headers for the CSV formatted according to the rules.
+      dict: A dictionary of headers for the CSV formatted according to the rules.
 
     If the rules specify custom headers for the CSV input, they will replace the default headers.
     Defaults to the headers defined in DEFAULT_HEADERS if no custom headers are found.
     """
     headers = {**DEFAULT_HEADERS}
     if 'input' in rules and 'csv' in rules['input']:
-        csv_rules = rules['input']['csv']
-        if 'header' in csv_rules:
-            headers.update(csv_rules['header'])
+      csv_rules = rules['input']['csv']
+      if 'header' in csv_rules:
+        headers.update(csv_rules['header'])
     return headers
--- app/common/xls_processor.py
+++ app/common/xls_processor.py
@@ -8,10 +8,10 @@ class XlsProcessor(BaseProcessor, ABC):
   A class to process Excel files based on specific rules defined in a rules file.
 
   Attributes:
-      rules (dict): The processing rules loaded from the rules file.
-      headers (dict): The headers to use for processing the transactions.
-      first_row (int): The row number from which to start reading the transactions.
-      transactions (pd.DataFrame): The loaded transactions from the input Excel file.
+    rules (dict): The processing rules loaded from the rules file.
+    headers (dict): The headers to use for processing the transactions.
+    first_row (int): The row number from which to start reading the transactions.
+    transactions (pd.DataFrame): The loaded transactions from the input Excel file.
   """
 
   def __init__(self, rules_file_path: Path, input_file_path: Path):
@@ -19,8 +19,8 @@ class XlsProcessor(BaseProcessor, ABC):
     Initializes the XlsProcessor with the provided rules file and input file paths.
 
     Args:
-        rules_file_path (Path): The file path to the rules file.
-        input_file_path (Path): The file path to the input Excel file.
+      rules_file_path (Path): The file path to the rules file.
+      input_file_path (Path): The file path to the input Excel file.
     """
     self.rules = self.load_rules(rules_file_path)
     self.headers = self.get_header(self.rules)
@@ -32,10 +32,10 @@ class XlsProcessor(BaseProcessor, ABC):
     Determines the first row to read from the Excel sheet based on the provided rules.
 
     Args:
-        rules (dict): The processing rules.
+      rules (dict): The processing rules.
 
     Returns:
-        int: The first row number to start reading from the sheet.
+      int: The first row number to start reading from the sheet.
     """
     if 'input' in rules and 'xls' in rules['input'] and 'sheet' in rules['input']['xls']:
       first_row = rules['input']['xls']['sheet'].get('first_row', 1)  # Default to 1 if not specified
@@ -47,13 +47,13 @@ class XlsProcessor(BaseProcessor, ABC):
     Loads the input Excel file into a DataFrame, skipping rows as specified.
 
     Args:
-        file_path (Path): The path to the input Excel file.
+      file_path (Path): The path to the input Excel file.
 
     Returns:
-        pd.DataFrame: The loaded transactions as a DataFrame.
+      pd.DataFrame: The loaded transactions as a DataFrame.
 
     Raises:
-        Exception: If there is an error loading the Excel file.
+      Exception: If there is an error loading the Excel file.
     """
     try:
       df = pd.read_excel(file_path, skiprows=self.first_row - 1)
@@ -66,12 +66,12 @@ class XlsProcessor(BaseProcessor, ABC):
     Retrieves the headers for the DataFrame based on the defined rules.
 
     Args:
-        rules (dict): The processing rules.
+      rules (dict): The processing rules.
 
     Returns:
-        dict: A dictionary containing the headers for the transactions.
+      dict: A dictionary containing the headers for the transactions.
     """
     headers = {**DEFAULT_HEADERS}
     if 'input' in rules and 'xls' in rules['input'] and 'header' in rules['input']['xls']:
-        headers.update(rules['input']['xls']['header'])
+      headers.update(rules['input']['xls']['header'])
     return headers
